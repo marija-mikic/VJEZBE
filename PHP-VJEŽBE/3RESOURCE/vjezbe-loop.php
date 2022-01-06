cb9e6bf72eed4ab0cac4bbb3c84cbbab85ea64e2
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-01-06 21:02:45 +0100

    vježbe -loop

---
+++ PHP-VJEŽBE/3RESOURCE/vjezbe-loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //napravite skriptu koja pokazuje 1-2-3-4-5-6-7-8-9-10 u jednom retku. neće biti crtice na početnoj i završnoj poziciji
+
+    for ($x=1;$x<10;$x++)
+    {
+        if ($x<10)
+    }
+    echo "$x-"
+    else
+    {
+        echo"$x"."/n"
+    }
+    ?>
+</body>
+</html>
