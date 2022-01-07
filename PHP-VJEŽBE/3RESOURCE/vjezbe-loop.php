562531b31d824a98f757beac65e97aea801a55cc
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-01-07 23:13:11 +0100

    Update vjezbe-loop.php

--- PHP-VJEŽBE/3RESOURCE/vjezbe-loop.php
+++ PHP-VJEŽBE/3RESOURCE/vjezbe-loop.php
@@ -46,9 +46,29 @@ for($x=1;$x<=5;$x++)
         echo " ";
     }
 }
-    echo "\n";
+    echo "<br>";
 
 }
+
+$n=5;
+for($i=1; $i<=$n; $i++)
+{
+for($j=1; $j<=$i; $j++)
+{
+echo ' * ';
+}
+echo "<br>";
+}
+for($i=$n; $i>=1; $i--)
+{
+for($j=1; $j<=$i; $j++)
+{
+echo " * ";
+}
+echo "<br>";
+}
+
+
 
 ?>
 </body>
