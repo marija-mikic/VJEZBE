eff083ce58b5dfe2147c4555522539d0ff3d5d07
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-01-08 12:13:25 +0100

    Update vjezbe-loop.php

--- PHP-VJEŽBE/3RESOURCE/vjezbe-loop.php
+++ PHP-VJEŽBE/3RESOURCE/vjezbe-loop.php
@@ -103,6 +103,33 @@ echo"fakrorijel od $n="."\nˇ"
      }
  }
 printf("\n");
+echo "<br>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//napišite skriptu koja kreira tablicu koristeći for petlju množenje .
+
+for($i=1;$i<=6;$i++)
+{
+    echo"<tr>";
+    for($j=1;$j<=5;$j++)
+}
+echo "<td>$i*$j=".$i*$j."</td>";
+}
+echo "</tr>";
+}
+ 
+?>
+</table> 
+    
 </body>
 </html>
